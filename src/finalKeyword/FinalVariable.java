5b27e30ff9cd769db7f75f7c71dd22b77007e8f4
yadav-bibhash <techxtor472@gmail.com>
2022-05-21 21:36:42 +0545

    homework done

--- src/finalKeyword/FinalVariable.java
+++ src/finalKeyword/FinalVariable.java
@@ -1,4 +1,8 @@
 package finalKeyword;
-
 public class FinalVariable {
+    public static void main(String[] args) {
+        final int Age = 35;
+//       int  age = 45;
+        System.out.println("Age: " + Age);
+    }
 }
--- src/loops/DoWhileLoop.java
+++ src/loops/DoWhileLoop.java
@@ -1,6 +1,12 @@
 package loops;
 
 public class DoWhileLoop {
-    // homework
-    // TODO
+    public static void main(String[] args) {
+        int i =  1;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i<=10);
+    }
 }
